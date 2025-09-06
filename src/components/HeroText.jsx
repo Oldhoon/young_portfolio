4e6d0c6ae9c2ca7540508db339300b0d1f23a5fe
Young <kimyoh123@gmail.com>
2025-09-06 13:53:57 -0700

    add hero section text

---
+++ src/components/HeroText.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const HeroText = () => {
+  return (
+    <div className='z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text'>
+        <div className='flex-col hidden md:flex c-space'>
+            <h1 className='text-4xl font-medium'>Hi I'm Young</h1>
+            <div className='flex flex-col items-start'>
+                <p className='text-5xl font-medium text-neutral-300'>
+                    An aspiring Software Engineer <br /> dedicated to crafting 
+                </p>
+                <p className='text-4xl font-medium text-neutral-300'>web solutions</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HeroText
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,10 @@
+import HeroText from "../components/HeroText"
+
 const Hero = () => {
   return (
-    <section className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space">Hero</section>
+    <section className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space">
+        <HeroText />
+    </section>
   )
 }
 
